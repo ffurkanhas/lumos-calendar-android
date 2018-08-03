701ff612b14001883d973e277eb59533ffdf05bf
Fatih Furkan Has <ffatihhas@gmail.com>
2018-08-04 01:10:25 +0300

    recurring events are added, listview is added

--- app/src/main/java/com/lumos/calendar/calendarandroid/MainActivity.java
+++ app/src/main/java/com/lumos/calendar/calendarandroid/MainActivity.java
@@ -1,16 +1,28 @@
 package com.lumos.calendar.calendarandroid;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import org.dmfs.rfc5545.recur.InvalidRecurrenceRuleException;
+import org.dmfs.rfc5545.recur.RecurrenceRule;
+import org.dmfs.rfc5545.recur.RecurrenceRuleIterator;
 import org.joda.time.DateTime;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.lumos.calendar.calendarandroid.util.EventListAdapter;
+import com.lumos.calendar.calendarandroid.util.FetchData;
 import com.yuyakaido.android.couplescalendar.model.CouplesCalendarEvent;
 import com.yuyakaido.android.couplescalendar.model.Theme;
 import com.yuyakaido.android.couplescalendar.ui.CouplesCalendarView;
@@ -20,6 +32,9 @@ import android.text.format.DateUtils;
 public class MainActivity extends Activity implements CouplesCalendarView.OnMonthChangeListener, CouplesCalendarView.OnDateClickListener {
 
     private TextView currentMonth;
+    private ListView mListView;
+    public static ArrayList<JSONObject> eventsJsonObjects;
+    public static ArrayList<SampleEvent> sampleEventList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +42,37 @@ public class MainActivity extends Activity implements CouplesCalendarView.OnMont
         setContentView(R.layout.activity_main);
 
         CouplesCalendarView view = findViewById(R.id.activity_main_couples_calendar_view);
+        mListView = findViewById(R.id.listView);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected item text from ListView
+                SampleEvent selectedItem = (SampleEvent) parent.getItemAtPosition(position);
+
+                // Display the selected item text on TextView
+                Toast.makeText(MainActivity.this, selectedItem.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         view.setOnMonthChangeListener(this);
         view.setOnDateClickListener(this);
-
-        view.setEvents(getDummyEvents());
 
         currentMonth = findViewById(R.id.current_month);
 
         int flags = DateUtils.FORMAT_SHOW_YEAR | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_NO_MONTH_DAY;
         Date month = new Date();
         currentMonth.setText(DateUtils.formatDateTime(this, month.getTime(), flags));
+
+        FetchData fetchData = new FetchData();
+        try {
+            Void fetch_result = fetchData.execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        view.setEvents(getEventsFromJsonData());
 
     }
 
@@ -50,23 +86,70 @@ public class MainActivity extends Activity implements CouplesCalendarView.OnMont
     @Override
     public void onDateClick(Date date) {
         int flags = DateUtils.FORMAT_SHOW_YEAR | DateUtils.FORMAT_SHOW_DATE;
-        String str = DateUtils.formatDateTime(this, date.getTime(), flags);
-        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
 
+        EventListAdapter adapter = new EventListAdapter(this, R.layout.adapter_view_layout, getSpecificDateEvents(date));
+        mListView.setAdapter(adapter);
     }
 
-    private List<CouplesCalendarEvent> getDummyEvents() {
+    private List<CouplesCalendarEvent> getEventsFromJsonData() {
         List<CouplesCalendarEvent> couplesCalendarEvents = new ArrayList<>();
+        sampleEventList = new ArrayList<>();
 
-        SampleEvent sampleEvent = new SampleEvent();
-        sampleEvent.setStartAt(new DateTime("2015-06-01T00:00:00.000Z").toDate());
-        sampleEvent.setEndAt(new DateTime("2015-06-01T01:00:00.000Z").toDate());
-        sampleEvent.setEventColor(getResources().getColor(Theme.RED.getEventColorId()));
-        sampleEvent.setRecurrenceRule("FREQ=WEEKLY");
-        couplesCalendarEvents.add(sampleEvent);
+        for (JSONObject eventJson : eventsJsonObjects) {
+            SampleEvent temp = new SampleEvent();
+            try {
+                temp.setId(eventJson.getInt("id"));
+                temp.setTitle(eventJson.getString("title"));
+                temp.setDescription(eventJson.getString("description"));
+                temp.setStartAt(new DateTime(eventJson.get("start_time")).toDate());
+                temp.setEndAt(new DateTime(eventJson.get("end_time")).toDate());
+                temp.setReminder(eventJson.getString("reminder"));
+                temp.setEventColor(getResources().getColor(Theme.RED.getEventColorId()));
+                if(!eventJson.isNull("recurring") && !eventJson.getString("recurring").equals("null")){
+                    RecurrenceRule rule = new RecurrenceRule(eventJson.getString("recurring"));
 
+                    org.dmfs.rfc5545.DateTime start = new org.dmfs.rfc5545.DateTime(new DateTime(eventJson.get("start_time")).getMillis());
+
+                    RecurrenceRuleIterator it = rule.iterator(start);
+
+                    while (it.hasNext()) {
+                        org.dmfs.rfc5545.DateTime nextInstance = it.nextDateTime();
+                        SampleEvent recurringEvent = (SampleEvent) temp.clone();
+                        recurringEvent.setStartAt(new DateTime(nextInstance.getTimestamp()).toDate());
+                        couplesCalendarEvents.add(recurringEvent);
+                        sampleEventList.add(recurringEvent);
+                    }
+                }
+                else{
+                    couplesCalendarEvents.add(temp);
+                    sampleEventList.add(temp);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (InvalidRecurrenceRuleException e) {
+                e.printStackTrace();
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+            }
+        }
         return couplesCalendarEvents;
     }
 
+    private ArrayList<SampleEvent> getSpecificDateEvents(Date selectedDate){
+        ArrayList<SampleEvent> sampleEvents = new ArrayList<>();
+
+        for(SampleEvent sampleEvent: sampleEventList){
+            DateTime dateTimeSelected = new DateTime(selectedDate);
+            DateTime dateTimeEvent = new DateTime(sampleEvent.getStartAt());
+            if(dateTimeSelected.getDayOfYear() == dateTimeEvent.getDayOfYear()
+                    && dateTimeSelected.getDayOfMonth() == dateTimeEvent.getDayOfMonth()
+                    && dateTimeSelected.getDayOfWeek() == dateTimeEvent.getDayOfWeek()){
+                sampleEvents.add(sampleEvent);
+            }
+        }
+
+        return sampleEvents;
+    }
 
 }
--- app/src/main/java/com/lumos/calendar/calendarandroid/SampleEvent.java
+++ app/src/main/java/com/lumos/calendar/calendarandroid/SampleEvent.java
@@ -7,12 +7,40 @@ import java.util.Date;
 /**
  * Created by yuyakaido on 9/12/15.
  */
-public class SampleEvent implements CouplesCalendarEvent {
+public class SampleEvent implements CouplesCalendarEvent, Cloneable {
 
+    private int id;
+    private String title;
+    private String description;
     private Date mStartAt;
     private Date mEndAt;
     private String mRRule;
+    private String reminder;
     private int mEventColor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public void setStartAt(Date startAt) {
         mStartAt = startAt;
@@ -41,6 +69,14 @@ public class SampleEvent implements CouplesCalendarEvent {
         return mRRule;
     }
 
+    public String getReminder() {
+        return reminder;
+    }
+
+    public void setReminder(String reminder) {
+        this.reminder = reminder;
+    }
+
     public void setEventColor(int eventColor) {
         mEventColor = eventColor;
     }
@@ -48,6 +84,10 @@ public class SampleEvent implements CouplesCalendarEvent {
     @Override
     public int getEventColor() {
         return mEventColor;
+    }
+
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 
 }
